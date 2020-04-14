6cb1aa5d3ce2f9929f07275ef6a8eabed1410bc9
AndyZhuravlev <andyzhuravlev@mail.ru>
2020-04-14 11:46:43 +0300

    change task_5.py

---
+++ lesson_2/practice_2/task_6.py
@@ -0,0 +1,71 @@
+
+# ОКЕИ — Общероссийский классификатор единиц измерения
+# ресурс https://classifikators.ru/okei
+
+unit_classifier = [{'id': 740, 'name': 'дюжина шт', 'description': 'Дюжина штук'},
+                   {'id': 778, 'name': 'упак', 'description': 'Упаковка'},
+                   {'id': 796, 'name': 'шт', 'description': 'Штука'}
+                   ]
+
+goods = []
+while True:
+    print('Введите информацию о товаре(для выхода введите "exit")')
+    name = ''
+    while True:
+        name = input('Введите наименование товара: ')
+        name = name.strip()
+        if name != 'exit' or name != '':
+            break
+        else:
+            print('Повторите ввод. Введена пустая строка!')
+    if name == 'exit':
+        break
+    price = -1
+    while True:
+        price_value = input('Введите цену товара: ')
+        if price_value == 'exit':
+            price = -1
+            break
+        if price_value.isdigit():
+            price = int(price_value)
+            break
+        print('Введите число!')
+    if price == -1:
+        break
+    count = 0
+    while True:
+        count_value = input('Введите количество товара на складе: ')
+        if count_value == 'exit':
+            count = 0
+            break
+        if count_value.isdigit():
+            count = int(count_value)
+            if count == 0:
+                print('Количество должно быть больше 0!')
+                continue
+            break
+        print('Введите число!')
+    if count == 0:
+        break
+    unit = -1
+    for itm in enumerate(unit_classifier, 1):
+        print(itm)
+    while True:
+        unit_id_value = input(f'Введите номер единицы измерения(от 1 до {len(unit_classifier)}): ')
+        if unit_id_value == 'exit':
+            unit = -1
+            break
+        if unit_id_value.isdigit():
+            unit = int(unit_id_value)
+            if unit == 0 or unit > len(unit_classifier):
+                continue
+            unit -= 1
+            break
+    if unit == -1:
+        break
+
+    goods.append((len(goods) + 1, {'наименование': name, 'цена': price, 'количество': count, 'ед': unit_classifier[unit]['name']}))
+
+if len(goods) > 0:
+    for itm in goods:
+        print(itm)
