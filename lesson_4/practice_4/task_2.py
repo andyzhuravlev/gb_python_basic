7b2f5484eecb03a0f6aad12832adda65388f94d0
AndyZhuravlev <andyzhuravlev@mail.ru>
2020-04-20 23:40:00 +0300

    edit task_1.py edit task_2.py

--- lesson_4/practice_4/task_2.py
+++ lesson_4/practice_4/task_2.py
@@ -3,3 +3,31 @@
 2. Представлен список чисел. Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
 Подсказка: элементы, удовлетворяющие условию, оформить в виде списка. Для формирования списка использовать генератор.
 """
+
+from random import uniform as uniform
+
+
+def get_list() -> list:
+    """
+
+    :return:
+    """
+
+    size_of_list = int(uniform(20, 30))
+
+    return [int(uniform(i, 100)) for i in range(0, size_of_list)]
+
+
+if __name__ == '__main__':
+
+    lis = get_list()
+    i = 0
+    print(lis)
+    while i < len(lis):
+        tmp = [itm for itm in lis[i:] if itm > lis[i]]
+        if len(tmp):
+            print(tmp)
+        i += 1
+
+
+
