38d72cb88e199e20e1793b08afb4179f1c842637
AndyZhuravlev <andyzhuravlev@mail.ru>
2020-04-21 12:21:34 +0300

    edit task_6.py

--- lesson_4/practice_4/task_6.py
+++ lesson_4/practice_4/task_6.py
@@ -5,3 +5,51 @@
 б) бесконечный итератор, повторяющий элементы некоторого списка, определенного заранее.
 Подсказка: использовать функцию count() и cycle() модуля itertools.
 """
+
+from itertools import count as count
+from itertools import cycle as cycle
+from my_gen import get_list as get_list
+
+
+def iter_1(start: int):
+    """
+
+    :param start:
+    :return:
+    """
+
+    for i in count(start):
+        print(i)
+
+
+def iter_2(lis: list):
+    """
+
+    :return:
+    """
+
+    for itm in cycle(lis):
+        print(itm)
+
+
+if __name__ == '__main__':
+
+    print(f'[0] - бесконечный итератор, генерирующий целые числа, начиная с указанного.\n'
+          f'[1] - бесконечный итератор, повторяющий элементы некоторого списка, определенного заранее.')
+
+    value = input('Выберите необходимый пункт: ')
+    if value.isdigit():
+        value = int(value)
+        if value == 0:
+            value = input('Введите нижний предел для генерации: ')
+            if value.isdigit():
+                iter_1(int(value))
+            else:
+                print('Это не число!')
+        elif value == 1:
+            lis = get_list(0, 10, 20)
+            iter_2(lis)
+        else:
+            print('Вариант не реализован!')
+    else:
+        print('Вариант не реализован!')
