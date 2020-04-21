10b09c7aa6b7eb91694ead36c44da1d93c7bf5f6
AndyZhuravlev <andyzhuravlev@mail.ru>
2020-04-21 10:42:34 +0300

    edit task_4.py

--- lesson_4/practice_4/task_4.py
+++ lesson_4/practice_4/task_4.py
@@ -4,3 +4,23 @@
 соответствующих требованию. Элементы вывести в порядке их следования в исходном списке. Для выполнения задания
 обязательно использовать генератор.
 """
+
+from random import uniform as uniform
+
+
+def get_list() -> list:
+    """
+
+    :return:
+    """
+
+    size_of_list = int(uniform(40, 50))
+
+    return [int(uniform(i, 30)) for i in range(0, size_of_list)]
+
+
+if __name__ == '__main__':
+
+    lis = get_list()
+    print(lis)
+    print([itm for itm in lis if lis[lis.index(itm) + 1:].count(itm) == 0])
