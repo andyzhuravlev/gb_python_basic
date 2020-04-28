fca2c9ad18900e2589f817420ff05d949c5038e5
AndyZhuravlev <andyzhuravlev@mail.ru>
2020-04-28 13:30:03 +0300

    edit task_4.py

--- lesson_6/practice_6/task_4.py
+++ lesson_6/practice_6/task_4.py
@@ -9,3 +9,116 @@
 Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+from time import sleep
+
+class Car:
+
+    speed: int
+    color: str
+    name: str
+    is_police: bool
+
+    def __init__(self, speed: int, color: str, name: str, is_police: bool = False):
+
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        """
+
+        :return:
+        """
+        print(f'Машина поехала {self.name} {self.color}')
+
+    def stop(self):
+        """
+
+        :return:
+        """
+        print('Машина остановилась')
+
+    def turn(self, direction: str):
+        """
+
+        :param direction:
+        :return:
+        """
+        print(f'Машина повернула {direction}')
+
+    def show_speed(self):
+        print(f'{self.speed}')
+
+
+class TownCar(Car):
+
+    def __init__(self, speed: int, color: str, name: str):
+
+        super().__init__(speed, color, name)
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Превышение скорости!')
+        else:
+            Car.show_speed(self)
+
+
+class SportCar(Car):
+
+    def __init__(self, speed: int, color: str, name: str):
+
+        super().__init__(speed, color, name)
+
+
+class WorkCar(Car):
+
+    def __init__(self, speed: int, color: str, name: str):
+        super().__init__(speed, color, name)
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('Превышение скорости!')
+        else:
+            Car.show_speed()
+
+
+class PoliceCar(Car):
+
+    def __init__(self, speed: int, color: str, name: str):
+        super().__init__(speed, color, name, True)
+
+
+if __name__ == "__main__":
+
+    tc = TownCar(40, 'Черный', 'Хундай солярис')
+    tc.go()
+
+    tc.show_speed()
+
+    tc.turn('направо')
+    tc.turn('направо')
+    tc.turn('налево')
+
+    tc.speed = 100
+    tc.show_speed()
+
+    tc.stop()
+
+    sleep(2)
+    print('')
+
+    sc = SportCar(60, 'Красный', 'Порше 911')
+    sc.go()
+
+    sc.show_speed()
+
+    sc.turn('налево')
+    sc.turn('налево')
+
+    sc.speed = 250
+    sc.show_speed()
+
+    sc.stop()
+
